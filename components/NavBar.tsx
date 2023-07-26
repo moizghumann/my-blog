8ebde4d5855c564264b9de5b562853f93e8cc6d0
moizghumann <moizghuman@gmail.com>
2023-07-27 00:52:59 +0500

    added z index to nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ import logo from '../public/images/moiz.jpg'
 
 const NavBar = () => {
     return (
-        <nav className='fixed w-full h-20 px-[6rem]'>
+        <nav className='fixed w-full h-20 px-[6rem] z-10'>
             <div className='flex flex-row justify-between items-center h-20 w-full px-6 ring-2 ring-[#E6E1C5] rounded-3xl mt-7 overflow-x-hidden'>
                 <div>
                     <Image
