88a4b254b97806382a96c8bff6bc996dd215ff9e
moizghumann <moizghuman@gmail.com>
2023-07-28 02:18:32 +0500

    customised motto badge

--- components/Motto.tsx
+++ components/Motto.tsx
@@ -4,9 +4,11 @@ import Section from './Section'
 const Motto = () => {
     return (
         <Section>
-            <div className='  flex flex-col justify-center items-center'>
+            <div className='  flex flex-col justify-center items-center h-screen'>
                 <div className='  bg-[#374151] flex flex-col justify-center items-center w-6/12'>
-                    <h3>Motto</h3>
+                    <div className=' bg-lime-400 rounded-lg flex flex-row justify-center items-center px-2'>
+                        <h3 className=' font-medium text-lime-950'>Motto</h3>
+                    </div>
                     <h1 className=' text-6xl font-semibold tracking-tight text-center'>Design experiences that people never forget</h1>
                 </div>
             </div>
