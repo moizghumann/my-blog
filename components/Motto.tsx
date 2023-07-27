574fc2e6f84ba5b7110f7fe182b28c77922f6692
moizghumann <moizghuman@gmail.com>
2023-07-28 02:22:09 +0500

    removed motto bg

--- components/Motto.tsx
+++ components/Motto.tsx
@@ -5,11 +5,11 @@ const Motto = () => {
     return (
         <Section>
             <div className='  flex flex-col justify-center items-center h-screen'>
-                <div className='  bg-[#374151] flex flex-col justify-center items-center w-6/12'>
+                <div className=' flex flex-col justify-center items-center w-9/12'>
                     <div className=' bg-lime-400 rounded-lg flex flex-row justify-center items-center px-2'>
                         <h3 className=' font-medium text-lime-950'>Motto</h3>
                     </div>
-                    <h1 className=' text-6xl font-semibold tracking-tight text-center'>Design experiences that people never forget</h1>
+                    <h1 className=' text-8xl font-semibold tracking-tight text-center'>Design experiences that people never forget</h1>
                 </div>
             </div>
         </Section>
