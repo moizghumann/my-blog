ab04a1e4a89a01ba78dc943fba9e6bfeb2e922b7
moizghumann <moizghuman@gmail.com>
2023-07-29 23:56:25 +0500

    changed github icon's height and width + spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ import copyright from '../public/copyright-svgrepo-com (3).svg'
 
 const Footer = () => {
     return (
-        <div className=' flex flex-col justify-center items-center mb-5'>
+        <div className=' flex flex-col justify-center items-center mb-5 mx-20'>
             <div className=' mx-10 w-full rounded-2xl px-5 mb-5 h-20 bg-[#B8DE1E] flex flex-row justify-between items-center'>
                 <div className='flex flex-row justify-center pl-1 items-center bg-[#0E1421] rounded-lg h-8 w-8'> <h1 className=' text-lg font-semibold text-[#B8DE1E]'>m.</h1></div>
 
@@ -20,13 +20,22 @@ const Footer = () => {
                         <Image src={linkedin} alt='twitter link' height={30} width={30} />
                     </Link>
                     <Link href={'https://github.com/moizghumann'}>
-                        <Image src={github} alt='twitter link' height={30} width={30} />
+                        <Image src={github} alt='twitter link' height={28} width={28} />
                     </Link>
                 </div>
             </div>
 
 
-            <p className=' flex items-center opacity-70 text-sm'>Copyright <Image src={copyright} alt='copright icon' height={15} width={15} /> Moiz's Portfolio 2023. Made with <Link href={'https://tailwindcss.com/'}>  Tailwindcss</Link>, <Link href={'https://nextjs.org/'}>Next.js and 💜 by Moiz Ghuman</Link></p>
+            <p className=' flex items-center opacity-70 text-sm'>
+                Copyright
+                <Image src={copyright} alt='copright icon' height={15} width={15} className='mx-[4px]' /> Moiz's Portfolio 2023. Made with
+                <Link href={'https://tailwindcss.com/'} className='ml-[4px]'>
+                    Tailwindcss,
+                </Link>
+                <Link href={'https://nextjs.org/'} className='ml-[4px]'>
+                    Next.js and 💜 by Moiz Ghuman
+                </Link>
+            </p>
 
 
         </div>
