5d4a9ba2c97a51b84738a425a8f9bc2839bb9ed8
moizghumann <moizghuman@gmail.com>
2023-08-01 00:58:23 +0500

    add home route to nav logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '../public/images/moiz.jpg'
+import Link from 'next/link'
 
 const NavBar = () => {
     return (
         <nav className='fixed w-full h-fit px-[6rem] z-10  mb-5'>
             <div className='flex flex-row justify-between items-center h-20 w-full px-6 ring-2 ring-[#E6E1C5] rounded-3xl mt-7 overflow-x-hidden backdrop-blur-sm'>
                 <div>
-                    <Image
-                        src={logo}
-                        alt='logo'
-                        height={50}
-                        width={50}
-                        className='cursor-pointer rounded-full'
-                        priority
-                    />
+                    <Link href={'/'}>
+                        <Image
+                            src={logo}
+                            alt='logo'
+                            height={50}
+                            width={50}
+                            className='cursor-pointer rounded-full'
+                            priority
+                        />
+                    </Link>
                 </div>
 
                 <div className=' flex items-center gap-6'>
