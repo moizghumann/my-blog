096ff1d42037579e05426c135ff5d54ee541f1fb
moizghumann <moizghuman@gmail.com>
2023-08-03 23:09:28 +0500

    fix spacing

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -48,26 +48,26 @@ const Showcase = () => {
                     </div>
                     <div className='grid grid-rows-2 gap-3 col-span-2'>
                         <div className='bg-[#db2777] rounded-xl'>
-                            <div className=' p-6 flex flex-col justify-around items-start '>
-                                <div className='flex flex-col justify-center items-start mb-5'>
-                                    <Image src={game} alt='game-logo' height={30} width={30} />
-                                    <h3 className='text-start font-semibold text-2xl text-[#0A0F19] py-1'>
-                                        GameVerse
-                                    </h3>
-                                    <p className='text-[#0a0f19b0] w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                </div>
+                            <div className=' p-6 flex flex-col justify-between items-start '>
+
+                                <Image src={game} alt='game-logo' height={30} width={30} />
+                                <h3 className='text-start font-semibold text-2xl text-[#0A0F19] pt-[3px] pb-[7px]'>
+                                    GameVerse
+                                </h3>
+                                <p className='text-[#0a0f19b0] w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
                                 <Button>info</Button>
                             </div>
                         </div>
                         <div className='bg-[#fb7185] rounded-xl'>
-                            <div className=' p-6 flex flex-col justify-around items-start '>
-                                <div className='flex flex-col justify-center items-start mb-5'>
-                                    <Image src={game} alt='game-logo' height={30} width={30} />
-                                    <h3 className='text-start font-semibold text-2xl text-[#0A0F19] py-1'>
-                                        GameVerse
-                                    </h3>
-                                    <p className='text-[#0a0f19b0] w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                </div>
+                            <div className=' p-6 flex flex-col justify-between items-start '>
+
+                                <Image src={game} alt='game-logo' height={30} width={30} />
+                                <h3 className='text-start font-semibold text-2xl text-[#0A0F19] pt-[3px] pb-[7px]'>
+                                    GameVerse
+                                </h3>
+                                <p className='text-[#0a0f19b0] w-[95%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
                                 <Button>info</Button>
                             </div>
                         </div>
